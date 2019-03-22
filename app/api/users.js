d016094ea33279ed029c09c9944af00664425d8b
Tomasz Prokop <tprokop@saba.com>
2019-03-22 09:22:13 +0100

    fix

--- app/api/users.js
+++ app/api/users.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import keys from "../../config/keys";
+import keys from "../config/keys";
 import passport from "passport";
 import { validateAddUserInput } from "../../validation/server/user";
 import { validateLoginInput } from "../../validation/server/login";
